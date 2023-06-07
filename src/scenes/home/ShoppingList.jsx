fac12dfe7adbc6d89b47b4fa0f6af633a68aa844
Dionisius Putra Widianto <64462297+dionisiuswidianto@users.noreply.github.com>
2023-06-07 21:57:57 +0700

    Update ShoppingList.jsx

--- src/scenes/home/ShoppingList.jsx
+++ src/scenes/home/ShoppingList.jsx
@@ -20,7 +20,7 @@ const ShoppingList = () => {
 
   async function getItems() {
     const items = await fetch(
-      "https://ecommerceserver-production.up.railway.app/api/items?populate=image",
+      "http://localhost:1337/api/items?populate=image",
       { method: "GET" }
     );
     const itemsJson = await items.json();
