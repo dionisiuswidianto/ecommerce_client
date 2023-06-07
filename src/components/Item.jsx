3668563da71f61d30b0d581313853f2ee898ac10
Dionisius Putra Widianto <64462297+dionisiuswidianto@users.noreply.github.com>
2023-06-07 21:49:05 +0700

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -38,7 +38,7 @@ const Item = ({ item, width }) => {
           alt={item.name}
           width="300px"
           height="400px"
-          src={`https://ecommerceserver-production.up.railway.app/${url}`}
+          src={`http://localhost:1337/${url}`}
           onClick={() => navigate(`/item/${item.id}`)}
           style={{ cursor: "pointer" }}
         />
